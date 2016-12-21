59abaa0badd2e95e353568bd022971b99f634e2d
Lisa Steele <lisasteele@Lisas-Mac-mini.local>
2016-12-20 22:00:41 -0800

    Flickr assignment. Accessing Flickr data and parsing JSON data using SwiftyJSON to extract the URL.

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -9,17 +9,76 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        let manager = AFHTTPSessionManager()
+        
+        let searchParameters:[String: Any] = ["method": "flickr.photos.search",
+                                              "api_key":  "9ab3bfcbb45d33d191797f08257e6651",
+                                              "format": "json",
+                                              "nojsoncallback": 1,
+                                              "text": "dogs",
+                                              "extras": "url_m",
+                                              "per_page": 5]
+        manager.get("https://api.flickr.com/services/rest/",
+                    parameters: searchParameters,
+                    progress: nil,
+                    success: { (operation: URLSessionDataTask, responseObject: Any?) in
+                        if let responseObject = responseObject {
+                            print ("Reponse: " + (responseObject as AnyObject).description)
+                            
+                            let jsondata = JSON(responseObject)
+                            
+                            if let flickrImageURL = jsondata["photos"]["photo"][0]["url_m"] as? AnyObject{
+                                print(flickrImageURL)
+                            }
+                            
 
-    override func didReceiveMemoryWarning() {
+                        }
+        }) { (operation: URLSessionDataTask?, error: Error) in
+            print ("Error: " + error.localizedDescription)
+        
+        }
+                        
+                   
+        
+}
+}
+
+
+
+
+
+      /*
+        if let photos = responseObject["photos"] as? [String: AnyObject] {
+        if let photoArray = photos["photo"] as? [[String: AnyObject]] {
+            self.scrollView.contentSize = CGSize(width: 320, height: 320 * CGFloat(photoArray.count))
+            for (i, photoDictionary) in photoArray.enumerate() {
+                if let imageURLString = photoDicitonary["url_m"] as? String: {
+                    let imageData = NSData(contentsOf: URL(string: imageURLString)!)
+                    if let imageDataUnwrapped = imageData {
+                        let imageView = UIIMageView(image: UIImage(data: imageDataUnwrapped as Data))
+                        imageView.frame = CGRect(x: 0, y: 320 * CGFloat(i), width: 320, height: 320)
+                        self.scrollView.addSubview(imageView)
+                    }
+                }
+            }
+            }
+            }
+        }
+    }
+ 
+        
+    
+        // Do any additional setup after loading the view, typically from a nib.
+    
+
+    /*override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-}
-
+ */*/
---
+++ bridge.h
@@ -0,0 +1,15 @@
+//
+//  bridge.h
+//  Photo Search Example
+//
+//  Created by Lisa Steele on 12/20/16.
+//  Copyright © 2016 Lisa Steele. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+
+
+#endif /* bridge_h */
+
+#import "AFNetworking/AFNetworking.h"
